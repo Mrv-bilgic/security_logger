dc79c89d573904f037e9b89e3c1cd97b82a367d2
Mrv-bilgic <mrvblgc@hotmail.com>
2026-03-09 18:21:58 +0300

    Initial Commit

---
+++ security_logger/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  security_logger
+//
+//  Created by Merve Bilgiç on 9.03.2026.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
